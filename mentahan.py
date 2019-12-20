03f6456742b80680ebea9239e266986c5469852f
Pausi Channel <43511729+404rgr@users.noreply.github.com>
2019-12-20 23:09:00 +0700

    Create mentahan.py

---
+++ mentahan.py
@@ -0,0 +1,33 @@
+# Good Luck Bro
+# Copyright 2019 Indonesian Hacker Rulez
+# coded lovingly by Zeerx7
+
+
+#Import Module
+import os
+import requests
+
+
+banner = """
+ Welcome To My Tools
+  Admin Finder
+ Coded By Zeerx7
+"""
+os.system('clear')
+print banner
+
+#input target
+target = raw_input('[Target]> ')
+
+#open wordlist
+f=open('/data/data/com.termux/files/home/Shell-Finder/dictionary','r')
+kontent=f.read()
+x=kontent.split('\n')
+
+for i in x:
+     url=target+"/"+i
+     code=requests.get(str(url)).status_code
+     if code == 200:
+             print "[+]Berhasil | url : "+url
+     else:
+             print "[-] Gagal | url:" + url
